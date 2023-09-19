7e6598d319f04ef2932231fe8d738a128d5fd27e
DansAlto <98467778+DansAlto@users.noreply.github.com>
2023-09-19 08:28:16 +0200

    Update FileHandler.js

--- FileHandler.js
+++ FileHandler.js
@@ -12,5 +12,14 @@ function GetItem(pathkey) {
                 let TextData = atob(data.content);
                 eval(TextData);
             });
+    }  else if(pathKey.includes(`.css`)) {
+        fetch(`https://api.github.com/repos/DansAlto/Cubes/contents/${Key}`)
+        .then((res) => res.json())
+        .then((data) => {
+            let dd = atob(data.content);
+            const newStyle = document.createElement(`style`);
+            newStyle.textContent = dd;
+            document.head.appendChild(newStyle);
+        })
     }
 }
